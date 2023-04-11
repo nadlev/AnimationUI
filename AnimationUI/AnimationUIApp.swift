daba9480cdad3372b17f636fe5346096cc80cfbb
nadlev <nadiya.levytska.jr@gmail.com>
2023-04-11 19:24:54 +0300

    Initial Commit

---
+++ AnimationUI/AnimationUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationUIApp.swift
+//  AnimationUI
+//
+//  Created by Надежда Левицкая on 4/11/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
